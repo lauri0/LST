155e4d0a913b01cf79cfe943a5991025be7ce6ff
Siiri Saar <siiri.saar1307@gmail.com>
2015-03-20 23:50:56 +0200

    Added SQL query for returning list of referees by the first letter of family name

--- src/main/java/Referee.java
+++ src/main/java/Referee.java
@@ -1,9 +1,7 @@
-/**
- * Created by siiri on 01/03/15.
- */
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Referee {
 
@@ -11,27 +9,15 @@ public class Referee {
     private String firstName;
     private String lastName;
     private String occupation;
+    private String email;
 
 
-    //static final String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
-    //static final String USER = "postgres";
-    //static final String PASS = "testParool123";
-
-    public Referee(int id, String firstName, String lastName, String occupation){
+    public Referee(int id, String firstName, String lastName, String occupation, String email){
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.occupation = occupation;
-    }
-
-    private static Connection getConnection() throws URISyntaxException, SQLException {
-        URI dbUri = new URI(System.getenv("DATABASE_URL"));
-
-        String username = dbUri.getUserInfo().split(":")[0];
-        String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
-
-        return DriverManager.getConnection(dbUrl, username, password);
+        this.email = email;
     }
 
     static Referee getRefereeByName(String firstName, String lastName){
@@ -40,23 +26,22 @@ public class Referee {
 
         try{
 
-            //Class.forName("org.postgresql.Driver");
-
-            //conn = DriverManager.getConnection(DB_URL,USER,PASS);
-            conn = getConnection();
+            conn = DatabaseConnection.getConnection();
 
             stmt = conn.createStatement();
             String sql;
-            System.out.println("SELECT id, first_name, last_name, occupation FROM referee WHERE first_name = " + "'" + firstName + "'" + " AND last_name = " + "'" + lastName + "'" + ";");
-            sql = "SELECT id, first_name, last_name, occupation FROM referee WHERE first_name = " + "'" + firstName + "'" + " AND last_name = " + "'" + lastName + "'" + ";";
+            System.out.println("SELECT id, first_name, last_name, occupation, email FROM referee WHERE first_name = " + "'" + firstName + "'" + " AND last_name = " + "'" + lastName + "'" + ";");
+            sql = "SELECT id, first_name, last_name, occupation, email FROM referee WHERE first_name = " + "'" + firstName + "'" + " AND last_name = " + "'" + lastName + "'" + ";";
             ResultSet rs = stmt.executeQuery(sql);
+
 
             if(rs.next()){
                 int id = rs.getInt("id");
                 String fName = rs.getString("first_name");
                 String lName = rs.getString("last_name");
                 String occupation = rs.getString("occupation");
-                Referee referee = new Referee(id, fName, lName, occupation);
+                String email = rs.getString("email");
+                Referee referee = new Referee(id, fName, lName, occupation, email);
                 return referee;
             }
 
@@ -92,6 +77,56 @@ public class Referee {
         return null;
     }
 
+    static List<Referee> getRefereesByFirstLetter(String firstLetter) {
+
+        List<Referee> referees = null;
+        Connection conn = null;
+        Statement stmt = null;
+
+        try {
+
+            conn = DatabaseConnection.getConnection();
+            referees = new ArrayList<Referee>();
+            stmt = conn.createStatement();
+            String sql;
+
+            System.out.println("SELECT * FROM referee WHERE last_name LIKE " + "'" + firstLetter + '%' + "'" + " ORDER BY last_name ASC;");
+
+            sql = "SELECT * FROM referee WHERE last_name LIKE " + "'" + firstLetter + '%' + "'" + " ORDER BY last_name ASC;";
+            ResultSet rs = stmt.executeQuery(sql);
+
+            while(rs.next()) {
+
+                int id = rs.getInt("id");
+                String fName = rs.getString("first_name");
+                String lName = rs.getString("last_name");
+                String occupation = rs.getString("occupation");
+                String email = rs.getString("email");
+                Referee referee = new Referee(id, fName, lName, occupation, email);
+
+                System.out.println(referee);
+
+                referees.add(referee);
+
+            }
+
+            stmt.close();
+            conn.close();
+
+            return referees;
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+
     public String getFirstName() {
         return firstName;
     }
@@ -106,6 +141,10 @@ public class Referee {
 
     public int getId() {
         return id;
+    }
+
+    public String getEmail() {
+        return email;
     }
 
     public String toString() {
