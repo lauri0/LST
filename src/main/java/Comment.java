bd26b46a51c72036486b8938b42fdcf1f5a91fe2
Siiri Saar <siiri.saar1307@gmail.com>
2015-03-22 01:02:12 +0200

    Added method for saving comment to database

--- src/main/java/Comment.java
+++ src/main/java/Comment.java
@@ -1,107 +1,77 @@
-
-/**
- * Created by siiri on 27/02/15.
- */
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.sql.*;
 
+
+
 public class Comment {
 
-    int id;
-    String firstName;
-    String lastName;
-    String occupation;
-    String message;
+    public static final int NOT_SAVED = -1;
 
-    //static final String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
-    //static final String USER = "postgres";
-    //static final String PASS = "testParool123";
+    private int id;
+    private String message;
 
 
-    public Comment(int id, String firstName, String lastName, String occupation, String message){
+    private Comment(int id, String message){
         this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.occupation = occupation;
         this.message = message;
     }
 
-    private static Connection getConnection() throws URISyntaxException, SQLException {
-        URI dbUri = new URI(System.getenv("DATABASE_URL"));
-
-        String username = dbUri.getUserInfo().split(":")[0];
-        String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
-
-        return DriverManager.getConnection(dbUrl, username, password);
+    public Comment(String message){
+        this.id = NOT_SAVED;
+        this.message = message;
     }
 
-    void saveToDatabase(){
+    public void save() throws ClassNotFoundException, SQLException, URISyntaxException {
+
+        Connection conn = null;
+        Statement stmt = null;
+
+        conn = DatabaseConnection.getConnection();
+        stmt = conn.createStatement();
+
+        if(this.id == NOT_SAVED){
+
+            stmt.execute("INSERT INTO comment (message) VALUES (" + " ' " + this.message + " ') " + " RETURNING id;");
+            ResultSet rs = stmt.getResultSet();
+            rs.next();
+            this.id = rs.getInt("id");
+
+        }
+        else {
+            stmt.executeUpdate("UPDATE comment SET message=" + " '" + this.message + "' " + " WHERE comment.id = " + " '" + this.id + "' " + ";");
+          
+        }
+
+    }
+
+
+    static Comment returnCommentByReferee(Referee ref, int electionId){
+
+        int refId = ref.getId();
 
         Connection conn = null;
         Statement stmt = null;
 
         try{
 
-            //Class.forName("org.postgresql.Driver");
-            //conn = DriverManager.getConnection(DB_URL,USER,PASS);
-            conn = getConnection();
-            //System.out.println(" INSERT INTO comments (id, first_name, last_name, occupation, comment_content) VALUES ("+Integer.toString(id)+",'" +firstName+"','"+lastName+"', '"+occupation+"','"+message+"');");
+            conn = DatabaseConnection.getConnection();
             stmt = conn.createStatement();
-            stmt.executeUpdate(" INSERT INTO comments (id, first_name, last_name, occupation, comment_content) VALUES ("+Integer.toString(id)+",'" +firstName+"','"+lastName+"', '"+occupation+"','"+message+"');");
 
-        stmt.close();
-        conn.close();
-
-    }catch(SQLException se){
-
-        se.printStackTrace();
-
-    }catch(Exception e){
-
-        e.printStackTrace();
-
-    }finally{
-
-        try{
-            if(stmt!=null)
-                stmt.close();
-        }catch(SQLException se2){
-        }
-        try{
-            if(conn!=null)
-                conn.close();
-        }catch(SQLException se){
-            se.printStackTrace();
-
-            }
-        }
-    }
-
-    static Comment getCommentByID(int number){
-
-        Connection conn = null;
-        Statement stmt = null;
-
-        try{
-
-            //conn = DriverManager.getConnection(DB_URL,USER,PASS);
-            conn = getConnection();
-            stmt = conn.createStatement();
-            String sql;
-            System.out.println("SELECT id, first_name, last_name, occupation, comment_content FROM comments WHERE id = " + Integer.toString(number) + ";");
-            sql = "SELECT id, first_name, last_name, occupation, comment_content FROM comments WHERE id = " + Integer.toString(number) + ";";
+            System.out.println("SELECT * FROM comment INNER JOIN referee_choices ON (comment.id = referee_choices.comment_id) WHERE referee_id = " + Integer.toString(refId) + " AND election_id = " + Integer.toString(electionId) + ";");
+            String sql = "SELECT * FROM comment INNER JOIN referee_choices ON (comment.id = referee_choices.comment_id) WHERE referee_id = " + Integer.toString(refId) + " AND election_id = " + Integer.toString(electionId) + ";";
             ResultSet rs = stmt.executeQuery(sql);
 
             if(rs.next()){
-                int id = rs.getInt("id");
-                String fName = rs.getString("first_name");
-                String lName = rs.getString("last_name");
-                String occupation = rs.getString("occupation");
-                String message = rs.getString("comment_content");
-                Comment comment = new Comment(id, fName, lName, occupation, message);
+
+                int commentId = rs.getInt("id");
+
+                String commentContent = rs.getString("message");
+
+                Comment comment = new Comment(commentId, commentContent);
+
                 return comment;
+
             }
 
             stmt.close();
@@ -109,40 +79,30 @@ public class Comment {
 
             return null;
 
-
-        }catch(SQLException se){
-
-            se.printStackTrace();
-
-        }catch(Exception e){
-
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
-
-        }finally{
-
-            try{
-                if(stmt!=null)
-                    stmt.close();
-            }catch(SQLException se2){
-            }
-            try{
-                if(conn!=null)
-                    conn.close();
-            }catch(SQLException se){
-                se.printStackTrace();
-            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
         }
 
         return null;
+    }
+
+
+    public String getMessage() {
+        return message;
+    }
+
+    public int getId() {
+        return id;
     }
 
     @Override
     public String toString() {
         return "Comment{" +
                 "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", occupation='" + occupation + '\'' +
                 ", message='" + message + '\'' +
                 '}';
     }
