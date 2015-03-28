9326911b01100c80b621b703e9aede0ed53d22ef
Siiri Saar <siiri.saar1307@gmail.com>
2015-03-29 00:51:58 +0200

    Seperated javascript code

---
+++ src/main/webapp/js/displayRefereeNames.js
@@ -0,0 +1,29 @@
+/**
+ * Created by siiri on 29/03/15.
+ */
+$(document).ready(function() {
+    $(".firstLetter").hide();
+    $("#alphabetically").click(function(){
+        //$(".firstLetter").slideToggle();
+        $('.firstLetter').animate({width: 'toggle'});
+        $("#tablediv").hide();
+        $(".firstLetter").click(function(event){
+            $.get("random",{letter: event.target.id},function(responseJson){
+                if(responseJson!=null){
+                    $("#tablediv").text("");
+                    for(var i=0;i<responseJson.length;i++){
+                        var obj = responseJson[i];
+                        var name = obj.firstName + " " + obj.lastName + ", " + obj.occupation;
+                        var n = document.createTextNode(name);
+                        var nameHolder = document.getElementById("tablediv");
+                        nameHolder.appendChild(n);
+                        nameHolder.appendChild(document.createElement("br"));
+                    }
+
+                    $("#tablediv").show();
+                }
+            });
+        });
+
+    });
+})
