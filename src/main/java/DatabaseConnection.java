0e54a55e55b738e09691b4db69ead1e676a755c7
lauri0 <lauri.kongas@hotmail.com>
2015-03-29 18:59:41 +0300

    Removed some duplicate code

--- src/main/java/DatabaseConnection.java
+++ src/main/java/DatabaseConnection.java
@@ -15,7 +15,6 @@ public class DatabaseConnection {
 
         URI dbUri = new URI(System.getenv("DATABASE_URL"));
 
-
         String username = dbUri.getUserInfo().split(":")[0];
         String password = dbUri.getUserInfo().split(":")[1];
         String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
