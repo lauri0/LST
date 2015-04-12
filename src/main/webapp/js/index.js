ac49ca3a0022f704e75da7920da3405a4f328a27
Siiri Saar <siiri.saar1307@gmail.com>
2015-04-12 19:52:42 +0300

    Added new js file for index page

---
+++ src/main/webapp/js/index.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+    $("#intro-text").hide();
+    $(".sign-toggle").click(function(){
+        var intro =  $("#intro-text");
+        var form = $("#form-and-text-container");
+        if($(this).hasClass("glyphicon-minus-sign")) {
+           var intro_text_height = parseInt(intro.css("height"));
+           form.css("padding-bottom",form.css("padding-bottom","-="+intro_text_height));
+           intro.hide();
+       }
+       else{
+           intro.show();
+           var intro_text_height = parseInt(intro.css("height"));
+           form.css("padding-bottom",form.css("padding-bottom","+="+intro_text_height));
+
+       }
+
+        $(this).toggleClass("glyphicon-plus-sign glyphicon-minus-sign");
+    })
+
+
+});
