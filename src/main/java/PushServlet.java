34402382c1df1e510daf946b6f17c6caee38f80d
Siiri Saar <siiri.saar1307@gmail.com>
2015-04-26 21:13:22 +0300

    Testing if works

--- src/main/java/PushServlet.java
+++ src/main/java/PushServlet.java
@@ -63,7 +63,7 @@ public class PushServlet extends HttpServlet {
             writer.flush();
 
             try {
-                Thread.sleep(5000);
+                Thread.sleep(50000);
             }
             catch (InterruptedException e) {
                 e.printStackTrace();
