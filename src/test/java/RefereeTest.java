bf63380a2e6fa020d8ca8ffe2181eac85394c866
Siiri Saar <siiri.saar1307@gmail.com>
2015-05-24 23:00:00 +0300

    Renamed unit test

--- src/test/java/RefereeTest.java
+++ src/test/java/RefereeTest.java
@@ -1,61 +1,80 @@
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class RefereeTest {
 
-    private WebDriver driver;
-
     @Before
     public void setUp() throws Exception {
 
-        this.driver = new FirefoxDriver();
-    }
-
-    @Test
-    public void displaysCorrectWindowName() throws Exception {
-
-        this.driver.get("https://nameless-hollows-9873.herokuapp.com/index");
-        this.driver.findElement(By.id("search-bar")).sendKeys("Mari Maasik, ajakirjanik");
-        this.driver.findElement(By.xpath("//span/button[text()='Mine']")).click();
-        assertEquals("Soovitaja valikud", this.driver.getTitle());
+       TestHelper.createDb();
 
     }
 
     @After
     public void tearDown() throws Exception {
 
-        this.driver.quit();
+        TestHelper.dropDb();
 
     }
 
     @Test
     public void testGetRefereeByName() throws Exception {
 
+        Referee mari = Referee.getRefereeByName("Mari", "Maasik");
+        assertEquals("ajakirjanik", mari.getOccupation());
+        assertEquals("mari.maasik@gmail.com", mari.getEmail());
     }
 
     @Test
     public void testGetRefereesByFirstNameLetter() throws Exception {
+
+        List<String> actualFirstNames = new ArrayList<>();
+        actualFirstNames.add("Mari");
+        actualFirstNames.add("Malle");
+
+        List<Referee> firstNames = Referee.getRefereesByFirstNameLetter("M");
+        for (int i = 0; i < firstNames.size(); i++) {
+             assertEquals(actualFirstNames.get(i), firstNames.get(i).getFirstName());
+        }
 
     }
 
     @Test
     public void testGetRefereesByFirstLetter() throws Exception {
 
+        List<Referee> lastNames = Referee.getRefereesByFirstLetter("M");
+        assertEquals(6, lastNames.size());
+
     }
 
     @Test
     public void testSave() throws Exception {
 
+        Referee mart = new Referee("Mart", "Männik", "kalevipoeg", "mart.mannik@ehitus.fi");
+        mart.save();
+        List<Referee> lastNames = Referee.getRefereesByFirstLetter("M");
+        assertEquals(7, lastNames.size());
+
     }
 
     @Test
     public void testSaveRefereeDataToDataBase() throws Exception {
+
+        Referee jaanus = new Referee("Jaanus", "Jõemaa", "näitleja", "jansu79@hot.ee");
+        Comment jaanusComment = new Comment("lore ipsum and all that jazz");
+
+        jaanus.saveRefereeDataToDataBase(jaanusComment, jaanus);
+        Comment returnedComment = Comment.returnCommentByReferee(jaanus, 1);
+        assertEquals(jaanusComment.getMessage(), returnedComment.getMessage());
 
     }
 }
